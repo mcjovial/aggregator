19cf60501abcb219d435ff358c587a1f87fb03c4
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-02-24 23:41:04 +0100

    created prelim pages and layouts for frontend

---
+++ frontend/src/views/Surveys.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageComponent from '../components/PageComponent'
+
+const Surveys = () => {
+  return (
+    <PageComponent title="Surveys" >
+      Children
+    </PageComponent>
+  )
+}
+
+export default Surveys
